be3bdda1121d66d266752c914dae258ba0545808
Liam Shatzel <liam.shatzel@gmail.com>
2025-02-28 03:19:04 -0800

    TODO: clean code 😭

--- run-script-cpu.sh
+++ run-script-cpu.sh
@@ -9,5 +9,5 @@
 module purge
 module load python/3.10 scipy-stack
 source venv/bin/activate
-python3 -m gns.train --data_path="datasets/WaterDropSample/" --ntraining_steps=10000 --wandb_sweep=True --wandb_enable=True --force_cpu=True
+python3 -m gns.train --data_path="datasets/WaterDropSample/" --ntraining_steps=1000000 --wandb_sweep=False --wandb_enable=True --force_cpu=True
 # python3 -m gns.train --data_path="datasets/WaterDropSample/" --ntraining_steps=10000 --wandb_sweep=False --wandb_enable=False --batch_size=32 --lr_init=0.00001
