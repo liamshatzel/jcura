0cc30d60f9e566b4d6ee1547cb0e8756f21d1cb4
Liam Shatzel <liam.shatzel@gmail.com>
2025-03-03 00:31:13 -0800

    Clean up

--- gns-simple/scripts/slurm/sandramps.sh
+++ gns-simple/scripts/slurm/sandramps.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-
-#SBATCH -J sandramps# Job name
-#SBATCH -o sandramps.o%j     # Name of stdout output file
-#SBATCH -e sandramps.e%j     # Name of stderr error file
-#SBATCH --gres=gpu:1
-#SBATCH -N 1                     # Total # of nodes (must be 1 for serial)
-#SBATCH -n 1                 # Total # of mpi tasks (should be 1 for serial)
-#SBATCH --mem=16G
-#SBATCH -t 12:00:00          # Run time (hh:mm:ss)
-#SBATCH --mail-user=<stevenbobyn@gmail.com>
+#SBATCH --account=def-brandonh
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=15:0:0
+#SBATCH --mail-user=lshatzel@uvic.ca
 #SBATCH --mail-type=ALL
 
 # fail on error
